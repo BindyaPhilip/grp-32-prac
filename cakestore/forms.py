28339d8203bd7e2b68504a04e6c38a9b48ec3a67
Bindya Philip <philbindya55@gmail.com>
2024-09-26 12:23:41 +0300

    made some changes in the forms.py

--- cakestore/forms.py
+++ cakestore/forms.py
@@ -1,22 +1,27 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django import forms
+from django.contrib.auth.forms import UserCreationForm  # Import the built-in UserCreationForm for user registration
+from django.contrib.auth.models import User  # Import the User model to use its fields
+from django import forms  # Import Django's forms module
 
-
+# Define a custom form for user registration that extends UserCreationForm
 class RegisterUserForm(UserCreationForm):
-	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-	last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-	
+    # Define an email field with a specific widget for styling
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    
+    # Define fields for first name and last name with specific styling
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
-	class Meta:
-		model = User
-		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+    class Meta:
+        # Specify the model to be used for the form
+        model = User
+        # Specify which fields to include in the form
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        # Call the parent class's constructor to initialize the form
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
 
-	def __init__(self, *args, **kwargs):
-		super(RegisterUserForm, self).__init__(*args, **kwargs)
-
-		self.fields['username'].widget.attrs['class'] = 'form-control'
-		self.fields['password1'].widget.attrs['class'] = 'form-control'
-		self.fields['password2'].widget.attrs['class'] = 'form-control'
+        # Add CSS class to the username, password1, and password2 fields for styling
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
